e91cc67cd28ea8be4544d410020dc826d74dfe4b
YBoyko <YBoyko@IBS.RU>
2021-09-14 11:44:18 +0300

    create

---
+++ zooclinic/zooclinic/src/main/java/com/zooclinic/zooclinic/ZooclinicApplication.java
@@ -0,0 +1,13 @@
+package com.zooclinic.zooclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooclinicApplication.class, args);
+	}
+
+}
---
+++ zooclinic/zooclinic/src/test/java/com/zooclinic/zooclinic/ZooclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooclinic.zooclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
